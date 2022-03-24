6914018b911a8446bb5e38f8607dfb8835a88989
k0881 <91438592+k0881@users.noreply.github.com>
2022-03-25 11:50:03 +1300

    Add files via upload

--- 06_Rounds_v01.py
+++ 06_Rounds_v01.py
@@ -1,3 +1,5 @@
+import random
+
 # set balance for testing purposes
 balance = 5
 
@@ -10,14 +12,48 @@ while play_again == "":
     rounds_played += 1
 
     # Print round number
-    print("*** Round #{} ***".format(rounds_played))
-    balance -= 1
-    print("Balance: ", balance)
     print()
+    print("*** Round #{} ***".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+# Adjust balance
+# if the random # is between 1 and 5
+# user gets a unicorn  (add $4 to balance)
+    if __name__ == '__main__':
+        if 1 <= chosen_num <= 5:
+            chosen = "unicorn"
+            balance += 4
+
+        # if the random # is between 6 and 36
+        # user gets a donkey (subtract $1 from balance)
+        elif 6 <= chosen_num <= 36:
+            chosen = "donkey"
+            balance -= 1
+
+        # The token is either a horse or zebra...
+        # in both cases, subtract $0.50 from the balance
+        else:
+            # if the number is even, set the chosen
+            # item to a horse
+            if chosen_num % 2 == 0:
+                chosen = "horse"
+
+            # otherwise set it to a zebra
+            else:
+                chosen = "zebra"
+            balance -= 0.5
+
+        print("You got a {}  Your balance is"
+              " ${:.2f}".format(chosen, balance))
 
     if balance < 1:
         play_again = "xxx"
         print("Sorry you have run out of money")
     else:
-        play_again = input("Press Enter to play again"
+        play_again = input("Press Enter to play again "
+                           ""
                            "or 'xxx' to quit")
+
+print()
+print("final balance is {$}")
